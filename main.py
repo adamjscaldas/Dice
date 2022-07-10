91783309cac949074d8bd96afcb9a49b4eb8c152
adamjscaldas <adamjscaldas@gmail.com>
2022-07-10 19:54:04 -0300

    Incluí o easygui no projeto e criei uma forma de rodar o código em loop

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import random
+import easygui
+
+
+def clean_number(number_to_clean: str) -> str:
+    items_removed = """ ,.+-=()_;:|\\/´`~^[]{}*&¨%$#@!?'"><
+    AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZzÇç"""
+    cleaned_number = number_to_clean
+    for x in range(len(items_removed)):
+        cleaned_number = cleaned_number.replace(items_removed[x], '')
+    return cleaned_number
 
 
 def cycling(cycles: int, random_value_used: list, dice_used: list) -> list:
@@ -7,59 +17,52 @@ def cycling(cycles: int, random_value_used: list, dice_used: list) -> list:
         random.shuffle(random_value_used)
         value_to_append = random.choice(dice_used)
         sequence_returned.append(value_to_append)
-    print(f"The sequence of dices rolled is: {sequence_returned}", end="\n\n")
     return sequence_returned
 
 
-def create_dict(dice_list_used: list) -> dict:
+def create_dict(dice_list_used: list, chosen_dict_to_use: dict, sequence: list) -> dict:
     for z in range(len(dice_list_used)):
         dice_value = dice_list_used[z]
-        count = 0
+        counting = 0
         while dice_value in sequence:
             sequence.remove(dice_value)
-            count += 1
-        if count > 0:
-            up_dict = {str(dice_value): count}
-            repetitions.update(up_dict)
+            counting += 1
+        if counting > 0:
+            up_dict = {str(dice_value): counting}
+            chosen_dict_to_use.update(up_dict)
         else:
             pass
-    return repetitions
+    return chosen_dict_to_use
 
 
-def check_appearance(dice_dict_used: dict) -> None:
-    print(f"""This was a cycle of {cycles} rolls.""", end="\n\n")
+def check_appearance(dice_dict_used: dict, cycles: int) -> str:
+    string_returned = ''
+
     for key in dict(dice_dict_used):
         key_value = dice_dict_used.get(key)
         if key_value != 0:
-            print(f""""{key}" was rolled {key_value} times""")
-    return None
+            string_returned += f""""{key}" was rolled {key_value} times\n"""
+    return string_returned
 
 
-d3_dict = {
-    '1': 0, '2': 0, '3': 0
-}
+def select_dice() -> str:
+    return easygui.buttonbox(msg="Qual dado você quer rodar?\n"
+                                 "Which dice do you want to roll?",
+                             choices=["d3", "d6", "d10", "d20", "d100"],
+                             title="ESCOLHER DADO / CHOOSE DICE")
+
+
 d3_list = [
     '1', '2', '3'
 ]
-
 d6_list = [
     '1', '2', '3',
     '4', '5', '6'
 ]
-d6_dict = {
-    '1': 0, '2': 0, '3': 0,
-    '4': 0, '5': 0, '6': 0
-}
-
 d10_list = [
     '1', '2', '3', '4', '5',
     '6', '7', '8', '9', '10'
 ]
-d10_dict = {
-    '1': 0, '2': 0, '3': 0, '4': 0, '5': 0,
-    '6': 0, '7': 0, '8': 0, '9': 0, '10': 0
-}
-
 d20_list = [
     '1', '2', '3', '4',
     '5', '6', '7', '8',
@@ -67,13 +70,6 @@ d20_list = [
     '13', '14', '15', '16',
     '17', '18', '19', '20'
 ]
-d20_dict = {
-    '1': 0, '2': 0, '3': 0, '4': 0, '5': 0,
-    '6': 0, '7': 0, '8': 0, '9': 0, '10': 0,
-    '11': 0, '12': 0, '13': 0, '14': 0, '15': 0,
-    '16': 0, '17': 0, '18': 0, '19': 0, '20': 0
-}
-
 d100_list = [
     '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
     '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
@@ -86,6 +82,23 @@ d100_list = [
     '81', '82', '83', '84', '85', '86', '87', '88', '89', '90',
     '91', '92', '93', '94', '95', '96', '97', '98', '99', '100'
 ]
+d3_dict = {
+    '1': 0, '2': 0, '3': 0
+}
+d6_dict = {
+    '1': 0, '2': 0, '3': 0,
+    '4': 0, '5': 0, '6': 0
+}
+d10_dict = {
+    '1': 0, '2': 0, '3': 0, '4': 0, '5': 0,
+    '6': 0, '7': 0, '8': 0, '9': 0, '10': 0
+}
+d20_dict = {
+    '1': 0, '2': 0, '3': 0, '4': 0, '5': 0,
+    '6': 0, '7': 0, '8': 0, '9': 0, '10': 0,
+    '11': 0, '12': 0, '13': 0, '14': 0, '15': 0,
+    '16': 0, '17': 0, '18': 0, '19': 0, '20': 0
+}
 d100_dict = {
     '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0,
     '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0,
@@ -99,25 +112,107 @@ d100_dict = {
     '91': 0, '92': 0, '93': 0, '94': 0, '95': 0, '96': 0, '97': 0, '98': 0, '99': 0, '100': 0
 }
 
-# dice e repetitions precisam ter o mesmo dado
-# dice and repetitions need to use the same dice
-dice = d20_list  # dice precisa ser list / dice needs to be a list
-repetitions = d20_dict  # repetitions precisa ser dict / repetitions needs to be a dict
+
+# ["d3", "d6", "d10", "d20", "d100"]
+def select_dice_dict(chosen_dice: str) -> dict:
+    while True:
+        if chosen_dice == "d3":
+            return d3_dict
+        elif chosen_dice == "d6":
+            return d6_dict
+        elif chosen_dice == "d10":
+            return d10_dict
+        elif chosen_dice == "d20":
+            return d20_dict
+        elif chosen_dice == "d100":
+            return d100_dict
+        else:
+            chosen_dice = select_dice()
 
 
-random_value = [dice]
-count = 0
+def select_dice_list(chosen_dice_dict: dict) -> list:
+    while True:
+        if chosen_dice_dict == d3_dict:
+            return d3_list
+        elif chosen_dice_dict == d6_dict:
+            return d6_list
+        elif chosen_dice_dict == d10_dict:
+            return d10_list
+        elif chosen_dice_dict == d20_dict:
+            return d20_list
+        elif chosen_dice_dict == d100_dict:
+            return d100_list
+        else:
+            chosen_dice_dict = select_dice()
 
 
-# Essa variável define quantas vezes o dado vai ser rolado
-# This variable defines how many times the dice will be rolled
-cycles = 20
-sequence = cycling(cycles=cycles, random_value_used=random_value, dice_used=dice)
+def roll_dice(cycles: int, random_value: list, chosen_list: list, chosen_dict: dict):
+    # Roda o dado e retorna uma lista com a ordem em que os números apareceram
+    # Rolls the dice and returns a list with the order each number appeared
+    sequence = cycling(cycles=cycles, random_value_used=random_value, dice_used=chosen_list)
 
-# Cria um dict com a quantidade de vezes que cada item apareceu
-# Creates a dict that saves how many times each number was rolled
-created_dict = create_dict(dice_list_used=dice)
+    # Cria um dict com a quantidade de vezes que cada item apareceu
+    # Creates a dict that saves how many times each number was rolled
+    created_dict = create_dict(dice_list_used=chosen_list, chosen_dict_to_use=chosen_dict, sequence=sequence)
 
-# Faz a checagem de quantas vezes cada número apareceu
-# Check how many times each number appeared
-check_appearance(dice_dict_used=repetitions)
+    # Faz a checagem de quantas vezes cada número apareceu
+    # Check how many times each number appeared
+    return check_appearance(dice_dict_used=created_dict, cycles=cycles)
+
+
+def game() -> bool:
+    choose_dice = select_dice()
+    if choose_dice is not None:
+        cycles = easygui.enterbox(msg=f"Quantas vezes o {choose_dice} será rodado?\n"
+                                      f"How many times will the {choose_dice} be rolled?",
+                                  title="CICLOS / CYCLES")
+        while True:
+            if cycles is not None:
+                try:
+                    cycles = int(clean_number(cycles))
+                    break
+                except ValueError:
+                    cycles = easygui.enterbox(msg=f"Quantas vezes o {choose_dice} será rodado?\n"
+                                                  f"How many times will the {choose_dice} be rolled?",
+                                              title="CICLOS / CYCLES")
+                except AttributeError:
+                    cycles = easygui.enterbox(msg=f"Quantas vezes o {choose_dice} será rodado?\n"
+                                                  f"How many times will the {choose_dice} be rolled?",
+                                              title="CICLOS / CYCLES")
+                finally:
+                    pass
+            else:
+                return False
+
+        chosen_dict = select_dice_dict(chosen_dice=choose_dice)
+        chosen_list = select_dice_list(chosen_dice_dict=chosen_dict)
+
+        random_value = [chosen_list]
+
+        numbers_ordered = roll_dice(cycles=cycles,
+                                    random_value=random_value,
+                                    chosen_list=chosen_list,
+                                    chosen_dict=chosen_dict)
+
+        easygui.msgbox(title="Resultado / Result",
+                       msg=numbers_ordered)
+        repeat = easygui.buttonbox(title="Repetir? / Repeat?",
+                                   msg="Rodar o dado novamente?\nRoll the dice again?",
+                                   choices=["Sim / Yes", "Não / No"])
+        if repeat == "Não / No":
+            return False
+        elif repeat == "Sim / Yes":
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    while True:
+        play = game()
+        if play is True:
+            pass
+        else:
+            break
